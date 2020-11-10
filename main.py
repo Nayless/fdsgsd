3b3ee663563875912e466ddbd8defb3ca51bc561
Ely <xbthbix@gmail.com>
2020-11-10 05:14:54 +0300

    code-2

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ import telebot;
 
 bot = telebot.TeleBot('1447603831:AAGbwnXcfhCRBzN6KCbjaBZVIE7nfSerz9A');
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('/help')
+keyboard1.row('Купить', 'Схема', 'Канал')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    keyboard1.row('Купить', 'Схема', 'Канал')
     if message.text == "/help":
-        bot.send_message(message.from_user.id, "Купить - информация о покупке        Схема - информация о схеме")
+        bot.send_message(message.from_user.id, "Купить - информация о покупке        Схема - информация о схеме        Канал - ссылка на канал")
     elif message.text == "Купить":
         bot.send_message(message.from_user.id, "Чтобы приобрести схему напиши @cXema_seller")
     elif message.text == "Схема":
-        bot.send_message(message.from_user.id, "Предпросмотр Схемы - https://drive.google.com/drive/folders/1kawUB8kXfudxu1hOKmfVrYlnEJQwRodh")
+        bot.send_message(message.from_user.id, "Предпросмотр Схемы https://drive.google.com/drive/folders/1kawUB8kXfudxu1hOKmfVrYlnEJQwRodh")
     elif message.text == "Канал":
         bot.send_message(message.from_user.id, "Вот ссылка на наш канал - @cXema_money")
     else:
