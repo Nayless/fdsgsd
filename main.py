7d79cd74a1dd24a4059771fd8d9d7fe15e605df8
Aissax <Aissax@users.noreply.github.com>
2020-11-10 03:29:58 +0300

    ‘fdsf’

---
+++ main.py
@@ -0,0 +1,19 @@
+import telebot;
+
+bot = telebot.TeleBot('1447603831:AAGbwnXcfhCRBzN6KCbjaBZVIE7nfSerz9A');
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard1.row('Купить', 'Схема', 'Канал')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "/help":
+        bot.send_message(message.from_user.id, "Купить - информация о покупке        Схема - информация о схеме")
+    elif message.text == "Купить":
+        bot.send_message(message.from_user.id, "Чтобы приобрести схему напиши @cXema_seller")
+    elif message.text == "Схема":
+        bot.send_message(message.from_user.id, "https://drive.google.com/drive/folders/1kawUB8kXfudxu1hOKmfVrYlnEJQwRodh")
+    elif message.text == "Канал":
+        bot.send_message(message.from_user.id, "Вот ссылка на наш канал - @cXema_money")
+    else:
+        bot.send_message(message.from_user.id, "Напиши /help если хочешь зарабатывать прямо сейчас", reply_markup=keyboard1)
+bot.polling(none_stop=True, interval=0)
